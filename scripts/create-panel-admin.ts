2f378fae897b3df19b33f9068a6d09acee1fff0d
Manishoo <maanishooshtari@gmail.com>
2019-08-07 11:59:27 +0430

    Fix create-panel-admin.ts incorrect variable

--- scripts/create-panel-admin.ts
+++ scripts/create-panel-admin.ts
@@ -3,17 +3,18 @@
  * Copyright: Ouranos Studio 2019. All rights reserved.
  */
 
-import 'reflect-metadata'
-const argv = require('minimist')(process.argv.slice(2))
 import { OperatorModel } from '@Models/operator.model'
 import { generateHashPassword } from '@Utils/password-manager'
-import { ROLE } from '@Types/common'
+import 'reflect-metadata'
+import { Role } from '../src/types/common'
+
+const argv = require('minimist')(process.argv.slice(2))
 
 async function main() {
 	const admin = new OperatorModel({
 		username: String(argv.u || 'admin'),
 		persistedPassword: await generateHashPassword(String(argv.p || '0000')),
-		role: ROLE.admin
+		role: Role.admin
 	})
 	await admin.save()
 }
