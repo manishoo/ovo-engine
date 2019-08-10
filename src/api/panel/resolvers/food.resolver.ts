62980327ff80c506cbf2bc5fb77d6899cffcb21b
Bamdad <nouri.bamdad@gmail.com>
2019-08-10 13:10:23 +0430

    Fix spacing

--- src/api/panel/resolvers/food.resolver.ts
+++ src/api/panel/resolvers/food.resolver.ts
@@ -13,30 +13,30 @@ import { Context } from '../utils'
 @Service()
 @Resolver()
 export default class FoodResolver {
-    constructor(
-        // service injection
-        private readonly foodService: FoodService
-    ) {
-        // noop
-    }
+	constructor(
+		// service injection
+		private readonly foodService: FoodService
+	) {
+		// noop
+	}
 
-    @Authorized(Role.operator)
-    @Query(returns => FoodsListResponse)
-    async foods(
-        @Arg('page', { defaultValue: 1 }) page: number,
-        @Arg('size', { defaultValue: 10 }) size: number,
-        @Ctx() ctx: Context,
-        @Arg('foodClassId', { nullable: true }) foodClassID?: string,
-    ) {
-        return this.foodService.listFoods(page, size, foodClassID)
-    }
+	@Authorized(Role.operator)
+	@Query(returns => FoodsListResponse)
+	async foods(
+		@Arg('page', { defaultValue: 1 }) page: number,
+		@Arg('size', { defaultValue: 10 }) size: number,
+		@Ctx() ctx: Context,
+		@Arg('foodClassId', { nullable: true }) foodClassID?: string,
+	) {
+		return this.foodService.listFoods(page, size, foodClassID)
+	}
 
-    @Authorized(Role.operator)
-    @Mutation(returns => Food)
-    async updateFood(
-        @Arg('food') food: FoodInput,
-        @Ctx() ctx: Context,
-    ) {
-        return this.foodService.updateFood(food)
-    }
+	@Authorized(Role.operator)
+	@Mutation(returns => Food)
+	async updateFood(
+		@Arg('food') food: FoodInput,
+		@Ctx() ctx: Context,
+	) {
+		return this.foodService.updateFood(food)
+	}
 }
