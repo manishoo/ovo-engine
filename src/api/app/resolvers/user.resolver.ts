7cf500be690fab250277a27dc9ee9357bf8a5c76
Manishoo <maanishooshtari@gmail.com>
2019-08-12 17:40:06 +0430

    Remove auth resolver

--- src/api/app/resolvers/user.resolver.ts
+++ src/api/app/resolvers/user.resolver.ts
@@ -4,8 +4,9 @@
  */
 
 import UserService from '@Services/user/user.service'
-import { User } from '@Types/user'
-import { Arg, Ctx, Query, Resolver } from 'type-graphql'
+import { GENDER, User } from '@Types/user'
+import userValidator from '@Utils/user.validator'
+import { Arg, Authorized, Ctx, Mutation, Query, Resolver } from 'type-graphql'
 import { Service } from 'typedi'
 import { Context } from '../utils'
 
@@ -20,8 +21,44 @@ export default class UserResolver {
     // noop
   }
 
+  @Mutation(returns => User)
+  async login(
+    @Arg('username') username: string,
+    @Arg('password') password: string,
+    @Ctx() ctx: Context
+  ): Promise<User> {
+    return this.userService.verifyUser(username, password)
+  }
+
+  @Mutation(returns => User)
+  async register(
+    @Arg('username') username: string,
+    @Arg('email') email: string,
+    @Arg('password') password: string,
+    @Arg('timeZone') timeZone: string,
+    @Ctx() ctx: Context,
+    @Arg('gender', { nullable: true }) gender?: GENDER,
+  ): Promise<User> {
+    const validatedData = userValidator.validateRegistration({
+      username,
+      email,
+      password,
+      timeZone,
+      gender,
+    })
+    return this.userService.createUser(validatedData.username, validatedData.email, validatedData.password, validatedData.timeZone, validatedData.gender)
+  }
+
+  @Authorized()
   @Query(returns => User)
-  async getUser(
+  async me(
+    @Ctx() ctx: Context,
+  ) {
+    return this.userService.getSelfUser(ctx.user!.id)
+  }
+
+  @Query(returns => User)
+  async user(
     @Arg('username') username: string,
     @Ctx() ctx: Context,
   ) {
