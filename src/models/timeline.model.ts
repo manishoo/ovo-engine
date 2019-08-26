af9396a53038d0d63a619be6088c8536194d31a0
Bamdad <nouri.bamdad@gmail.com>
2019-08-26 17:05:21 +0430

    Add timeline model and type

---
+++ src/models/timeline.model.ts
@@ -0,0 +1,36 @@
+
+import mongooseDelete, { SoftDeleteDocument, SoftDeleteModel } from 'mongoose-delete'
+import { plugin, prop, Ref, Typegoose } from 'typegoose'
+import { UserSchema } from './user.model'
+import { TimeLine, TimelineMeal } from '@Types/timeline'
+import mongoose from '@Config/connections/mongoose'
+
+
+export interface TimelineSchema extends SoftDeleteModel<SoftDeleteDocument> {
+}
+
+@plugin(mongooseDelete, {
+  deletedAt: true,
+  deletedBy: true,
+  overrideMethods: true,
+})
+export class TimelineSchema extends Typegoose implements TimeLine {
+  @prop()
+  date: Date
+
+  @prop({ ref: UserSchema })
+  user: Ref<UserSchema>
+
+  @prop()
+  meals: TimelineMeal[]
+}
+
+export const TimelineModel = new TimelineSchema().getModelForClass(TimelineSchema, {
+  existingMongoose: mongoose,
+  schemaOptions: {
+    collection: 'timeline',
+    timestamps: true,
+    emitIndexErrors: true,
+    validateBeforeSave: true,
+  }
+})
---
+++ src/types/timeline.ts
@@ -0,0 +1,59 @@
+import { ObjectType, Field, registerEnumType, InputType } from "type-graphql"
+import { UserSchema } from "@Models/user.model"
+import { Ref } from "typegoose"
+import { User } from "@Types/user"
+import { DishSchema } from "@Models/dish.model"
+import { Dish } from "./dish"
+
+
+export enum TimelineMealType {
+  breakfast = 'breakfast',
+  lunch = 'lunch',
+  dinner = 'dinner',
+  snack = 'snack',
+}
+
+registerEnumType(TimelineMealType, {
+  name: 'TimelineMealType',
+  description: 'Timeline meal types'
+})
+
+@ObjectType()
+export class TimelineMeal {
+
+  @Field(type => TimelineMealType)
+  type: TimelineMealType
+
+  @Field()
+  time: Date
+
+  @Field(type => Dish)
+  dish: Ref<DishSchema>
+}
+
+@InputType()
+export class TimelineMealInput {
+
+  @Field(type => TimelineMealType)
+  type: TimelineMealType
+
+  @Field(type => Date)
+  time: Date
+
+  @Field(type => Dish)
+  dish: Ref<DishSchema>
+}
+
+@ObjectType()
+export class TimeLine {
+
+  @Field(type => Date)
+  date: Date
+
+  @Field(type => User)
+  user: Ref<UserSchema>
+
+  @Field(type => [TimelineMeal])
+  meals: TimelineMeal[]
+}
+
