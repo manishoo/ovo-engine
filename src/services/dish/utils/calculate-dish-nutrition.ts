c1afccb1f6165098f1371adc4899b24e72f17e26
Bamdad <nouri.bamdad@gmail.com>
2019-09-05 16:29:27 +0430

    Optimize calculate dish nutrition

--- src/services/dish/utils/calculate-dish-nutrition.ts
+++ src/services/dish/utils/calculate-dish-nutrition.ts
@@ -1,5 +1,5 @@
 import { DishItem } from "@Types/dish"
-import { Nutrition, Food } from "@Types/food"
+import { Nutrition, Food, NutrientUnit } from "@Types/food"
 import { Recipe } from "@Types/recipe"
 
 
@@ -8,60 +8,62 @@ export function calculateDishNutrition(items: DishItem[]): Nutrition {
 
   items.map(dishItem => {
 
+    function baseNutritionAmount(totalNutritionFieldName: NutrientUnit | undefined): number {
+      if (totalNutritionFieldName) {
+        return totalNutritionFieldName.amount
+      }
+      return 0
+    }
+
+    function convertCalorie(cal: NutrientUnit): number {
+      if (cal.unit == 'kJ') {
+        return cal.amount * 239
+      } else {
+        return cal.amount
+      }
+    }
+
     function calc(fieldName: string) {
       if (dishItem.recipe) {
         let recipe = dishItem.recipe as Recipe
         if (recipe.nutrition && recipe.nutrition[fieldName]) {
-          let baseNut = 0
-          if (totalNutrition[fieldName]) {
-            baseNut = totalNutrition[fieldName]!.amount
-          }
+
           totalNutrition[fieldName] = {
-            amount: baseNut + recipe.nutrition[fieldName]!.amount * dishItem.amount,
+            amount: baseNutritionAmount(totalNutrition[fieldName]) + recipe.nutrition[fieldName]!.amount * dishItem.amount,
             unit: recipe.nutrition[fieldName]!.unit,
           }
         }
       } else {
         let food = dishItem.food as Food
         if (food.nutrition && food.nutrition[fieldName]) {
-          let baseNut = 0
-          if (totalNutrition[fieldName]) {
-            baseNut = totalNutrition[fieldName]!.amount
-          }
+
           totalNutrition[fieldName] = {
-            amount: baseNut + (food.nutrition[fieldName]!.amount / 100 * dishItem.amount),
+            amount: baseNutritionAmount(totalNutrition[fieldName]) + (food.nutrition[fieldName]!.amount / 100 * dishItem.amount),
             unit: food.nutrition[fieldName]!.unit,
           }
         }
       }
     }
 
-    function calculateCalories() {
-      if (dishItem.recipe) {
+    function calculateFoodCalories() {
 
+      if (dishItem.recipe) {
         let recipe = dishItem.recipe as Recipe
 
         if (recipe.nutrition && recipe.nutrition.calories) {
-          let baseCal = 0
-          if (totalNutrition.calories) {
-            baseCal = totalNutrition.calories.amount
-          }
+
           totalNutrition.calories = {
-            amount: baseCal + recipe.nutrition.calories.amount * dishItem.amount,
+            amount: baseNutritionAmount(totalNutrition.calories) + convertCalorie(recipe.nutrition.calories) * dishItem.amount,
             unit: 'kcal',
           }
         }
       } else {
-
         let food = dishItem.food as Food
 
         if (food.nutrition && food.nutrition.calories) {
-          let baseCal = 0
-          if (totalNutrition.calories) {
-            baseCal = totalNutrition.calories.amount
-          }
+
           totalNutrition.calories = {
-            amount: baseCal + (food.nutrition.calories.amount / 100 * dishItem.amount * food.weights.find(w => w.id == dishItem.weight)!.gramWeight),
+            amount: baseNutritionAmount(totalNutrition.calories) + (convertCalorie(food.nutrition.calories) / 100 * dishItem.amount * food.weights.find(w => w.id == dishItem.weight)!.gramWeight),
             unit: 'kcal'
           }
         }
@@ -72,10 +74,8 @@ export function calculateDishNutrition(items: DishItem[]): Nutrition {
     dishItem.recipe ? rf = dishItem.recipe as Recipe : rf = dishItem.food as Food
     Object.keys(rf!.nutrition!).map(nutritionName => calc(nutritionName))
 
-    calculateCalories()
-
+    calculateFoodCalories()
   })
 
   return totalNutrition
 }
-
