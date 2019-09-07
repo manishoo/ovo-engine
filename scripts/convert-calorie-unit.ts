64192af02610b7822fad1050bc48611321f14bde
Bamdad <nouri.bamdad@gmail.com>
2019-09-07 16:50:54 +0430

    Add kJ to kcal convertor script

---
+++ scripts/convert-calorie-unit.ts
@@ -0,0 +1,36 @@
+import { FoodModel, FoodSchema } from '../src/models/food.model'
+import { InstanceType } from 'typegoose'
+
+
+async function convertCalorieUnit(food: InstanceType<FoodSchema>) {
+  if (food.nutrition.calories && food.nutrition.calories.unit == 'kJ') {
+    let kjCalorie = food.nutrition.calories.amount
+
+    food.nutrition = {
+      ...food.nutrition,
+      calories: {
+        amount: kjCalorie / 4.18,
+        unit: 'kcal',
+      }
+    }
+
+    await food.save()
+    console.log(food.id)
+  }
+}
+
+async function main() {
+  console.log('Script started running.')
+
+  let foodList = await FoodModel.find()
+
+  await Promise.all(foodList.map(async food => {
+    await convertCalorieUnit(food)
+  }))
+}
+
+main().then(() => {
+  console.log('Script done running.')
+  process.exit(0)
+})
+
