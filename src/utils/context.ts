395891bef5e82f336d84779773f51f641e461378
Manishoo <maanishooshtari@gmail.com>
2019-09-13 12:49:52 +0430

    Add access to recipe crud for operator users

--- src/utils/context.ts
+++ src/utils/context.ts
@@ -6,13 +6,15 @@
 import { LanguageCode, Status, UserRole, OperatorRole } from '@Types/common'
 import { Request } from 'express'
 
+export interface ContextUser {
+  id: string,
+  status: Status,
+  lang: LanguageCode,
+  role: UserRole | OperatorRole
+}
+
 export interface Context {
   request: Request,
   lang: LanguageCode,
-  user?: {
-    id: string,
-    status: Status,
-    lang: LanguageCode,
-    role: UserRole | OperatorRole
-  },
+  user?: ContextUser,
 }
