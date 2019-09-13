58ccd64da2504aec14128cfc04c55c45870fd42d
Manishoo <maanishooshtari@gmail.com>
2019-09-13 12:50:03 +0430

    Add resolve-time middleware

---
+++ src/api/common/middlewares/resolve-time.middleware.ts
@@ -0,0 +1,13 @@
+/*
+ * resolve-time.middleware.ts
+ * Copyright: Ouranos Studio 2019. All rights reserved.
+ */
+
+import { MiddlewareFn } from 'type-graphql'
+
+export const ResolveTime: MiddlewareFn = async ({ info }, next) => {
+  const start = Date.now()
+  await next()
+  const resolveTime = Date.now() - start
+  console.log(`${info.parentType.name}.${info.fieldName} [${resolveTime} ms]`)
+}
