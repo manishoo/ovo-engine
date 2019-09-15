485e637c7f78818b678a494b4845ae1fffdaf2d4
Manishoo <maanishooshtari@gmail.com>
2019-09-15 18:02:18 +0430

    Add downloading foodb images script

--- scripts/attach-foodb-images-to-db.ts
+++ scripts/attach-foodb-images-to-db.ts
@@ -3,21 +3,48 @@
  * Copyright: Ouranos Studio 2019. All rights reserved.
  */
 
-
-
+import config from '../src/config'
 import { FoodClassModel } from '../src/models/food-class.model'
 
+
+const download = require('image-downloader')
+const fs = require('fs-extra')
+
 export default async function main() {
-	const foodClasses = await FoodClassModel.find()
+  const foodClasses = await FoodClassModel.find()
+  const errors: any[] = []
 
-	await Promise.all(foodClasses.map(async fc => {
-		fc.imageUrl = {
-			url: `http://foodb.ca/system/foods/pictures/${fc.origId}/full/${fc.origId}.png`
-		}
-		fc.thumbnailUrl = {
-			url: `http://foodb.ca/system/foods/pictures/${fc.origId}/thumb/${fc.origId}.png`
-		}
+  for (let fc of foodClasses) {
+    const basePath = `${config.uploadUrl}/images/food-classes/${fc.slug}`
+    await fs.ensureDirSync(basePath)
 
-		await fc.save()
-	}))
+    try {
+      const {filename} = await download.image({
+        url: `http://foodb.ca/system/foods/pictures/${fc.origId}/full/${fc.origId}.png`,
+        dest: `${basePath}/full.png`,
+      })
+      fc.imageUrl = {url: filename}
+    } catch (e) {
+      console.log(e)
+      errors.push(e)
+    }
+    try {
+      const {filename} = await download.image({
+        url: `http://foodb.ca/system/foods/pictures/${fc.origId}/thumb/${fc.origId}.png`,
+        dest: `${basePath}/thumb.png`,
+      })
+      fc.thumbnailUrl = {url: filename}
+    } catch (e) {
+      console.log(e)
+      errors.push(e)
+    }
+
+    await fc.save()
+    process.stdout.write('.')
+  }
+
+  fs.writeFileSync(
+    './errors.json',
+    JSON.stringify(errors),
+    { encoding: 'utf8' })
 }
