aa286d73cf92747e4274f1d26be8a3dca81e46c7
Bamdad <nouri.bamdad@gmail.com>
2019-10-03 16:31:18 +0330

    Remove unnecessary comments

--- src/api/app/resolvers/calendar.resolver.ts
+++ src/api/app/resolvers/calendar.resolver.ts
@@ -58,58 +58,5 @@ export default class CalendarResolver {
     @Ctx() ctx: Context,
   ) {
     return this.calendarService.logMeasurements(bodyMeasurements, ctx.user!.id)
-    /*
-    return [
-      {
-        id: '5d667c739f5aa96618af5b94',
-        date: new Date('2019-09-23T14:12:28.098Z'),
-        user: {
-          id: '5d667c739f5aa96618af5b94',
-          username: 'Gholam ali',
-        },
-        meals: [{
-          type: MealType.breakfast,
-          time: new Date('2019-09-23T14:12:28.098Z'),
-          items: [{
-            amount: 4,
-            recipe: {
-              id: '5d667c739f5aa96618af5b93',
-              title: [{ locale: LanguageCode.en, text: 'cheshm' }],
-              ingredients: [
-                {
-                  food: {
-                    id: '5d667c739f5aa96618af5b87',
-                    name: [{ locale: LanguageCode.en, text: 'carrot' }],
-                  },
-                  weight: '5d667c739f5aa96618af5b94'
-                }
-              ],
-              serving: 1,
-              slug: 'die-hard-please-vyHCB2D',
-            }
-          }]
-        }],
-        activities: [
-          {
-            id: '5d6cd273eff1e93a034aeb5b',
-            duration: 12,
-            activityTypeName: [{ locale: LanguageCode.en, text: 'MTB' }],
-            totalBurnt: 398,
-            activityName: 'Morning ride',
-            time: new Date('2019-10-23T14:12:28.098Z'),
-            met: 4,
-            activityGroup: {
-              name: [{ locale: LanguageCode.en, text: 'Cycling' }]
-            }
-          }
-        ],
-        totalBurnt: 4,
-        measurements: {
-          time: new Date('2019-10-23T14:12:28.098Z'),
-          weight: 132,
-        }
-      }
-    ]
-     */
   }
 }
